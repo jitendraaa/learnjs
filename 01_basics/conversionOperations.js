1426f7d14f68efcb2138c059a8867359f4f3b631
OPENTEXT\ajitendra <ajitendra@opentext.com>
2024-08-26 21:46:03 +0530

    Create conversionOperations.js

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,36 @@
+/**let score = 33
+score = null // after converted to Number = 0
+score = undefined //after converted to Number = NaN
+score = true // after converted to Number  = 1
+
+console.log(typeof(score));
+
+
+let scoreInNumber = Number(score)
+
+console.log(typeof(scoreInNumber));
+console.log(scoreInNumber);
+*/
+
+
+let isLoggedIn = "1"
+/*
+ * Boolean
+ * 1 > true
+ * 0 > false
+ * any other number positive and negative > true
+ * "" > false
+ * "anything here" > true
+ * null > false
+ * undefined > false
+ */
+isLoggedIn = null
+isLoggedIn = undefined
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+
+
+
+
+
