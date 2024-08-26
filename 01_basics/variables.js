8dcdb11e55b3266dad1d72f92c4d81ac7a7a212b
OPENTEXT\ajitendra <ajitendra@opentext.com>
2024-08-26 21:16:45 +0530

    Update variables.js
    learning variables

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,5 +1,17 @@
 const accountID = 5463218
 let accountEmail = "hello@gmail.com"
+/**
+ * var has a problem with Scope, So this gave birth to let.
+ * So prefer not to use VAR. because issue in block Scope/Functional Scope.
+ * 
+ */
 var accountPassword = "12314"
 accountCity = "Vizag"
+let accountState;
 
+// accountID = 1
+
+console.log(accountID)
+
+
+console.table([accountID, accountEmail, accountPassword, accountCity, accountState])
